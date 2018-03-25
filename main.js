87c061d468656e5285d8824631509b02abde3b2d
Ayrton Sparling <ayrton@sparling.us>
2018-03-24 17:24:07 -0700

    Support passing logger to constructor

--- main.js
+++ main.js
@@ -8,66 +8,86 @@ const onDeath = require('death');
 const Path = require('path');
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs-extra');
-const Promise = require('bluebird').config({
-    cancellation: true
-});
+const Promise = require('bluebird');
 
 module.exports = class Sample extends nmmes.Module {
-    constructor(args) {
+    constructor(args, logger = Logger) {
         super(require('./package.json'));
+        this.logger = logger;
 
         this.options = Object.assign(nmmes.Module.defaults(Sample), args);
 
         this.options.length = this.options.length / 1000; // Convert milliseconds to seconds
     }
-    executable(map) {
+    async executable(map) {
         let _self = this;
         let options = this.options;
         let video = this.video;
 
         this.removeDeathListener = onDeath(function(signal, err) {
-            Logger.trace('Signal receieved:', signal, err);
-            _self.encoder.kill(signal);
+            if (_self.encoder) {
+                _self.trace('Signal receieved:', signal, err);
+                _self.encoder.kill(signal);
+            }
         });
+        if (options.screenshots) {
+            let encoder = this.encoder = ffmpeg(video.output.path).outputOptions('-map', '0');
+            await (new Promise((resolve, reject) => {
+                encoder
+                    .on('error', function(error, stdout, stderr) {
+                        _self.removeDeathListener();
+                        reject(error);
+                    })
+                    .on('end', function(stdout, stderr) {
+                        _self.removeDeathListener();
+                        resolve();
+                    })
+                    .screenshots({
+                        timestamps: ['50%', '70%'],
+                        filename: 'thumbnail-at-%s-seconds.png',
+                        folder: Path.resolve(this.output.dir, 'screenshots')
+                    });
+            }));
+        }
 
-        let encoder = this.encoder = ffmpeg(video.output.path).outputOptions('-c', 'copy').outputOptions('-map', '0');
-        return Promise.props({
-            seek: Sample.calculateSeek(options.seek, options.length, video),
-            output: Sample.generateOutputPath(options.output, video.output.path, options.suffix)
-        }).then(props => {
-            encoder.seekInput(Math.min(video.output.metadata.format.duration - props.seek, Math.max(0, props.seek)));
+        if (options.sample) {
+            let encoder = this.encoder = ffmpeg(video.output.path).outputOptions('-c', 'copy').outputOptions('-map', '0');
+            await Promise.props({
+                seek: Sample.calculateSeek(options.seek, options.length, video),
+                output: Sample.generateOutputPath(options.output, video.output.path, options.suffix)
+            }).then(props => {
 
-            encoder.duration(options.length);
+                encoder.seekInput(Math.min(video.output.metadata.format.duration - props.seek, Math.max(0, props.seek)));
 
-            // TODO: Enable format option
-            encoder.format('matroska');
+                encoder.duration(options.length);
 
-            encoder.output(props.output);
+                // TODO: Enable format option
+                encoder.format('matroska');
 
-            return new Promise(function(resolve, reject, onCancel) {
+                encoder.output(props.output);
 
-                fs.ensureDir(Path.dirname(props.output), err => {
-                    if (err)
-                        return reject(err);
+                return new Promise(function(resolve, reject) {
 
-                    _self.encoder
-                        .on('start', function(commandLine) {
-                            Logger.trace('[FFMPEG] Query:', commandLine);
-                        })
-                        .on('error', function(error, stdout, stderr) {
-                            Logger.debug('[FFMPEG] STDOUT:\n', stdout, '[FFMPEG] STDERR:\n', stderr);
-                            _self.removeDeathListener();
-                            reject(error);
-                        })
-                        .on('end', function(stdout, stderr) {
-                            _self.removeDeathListener();
-                            resolve();
-                        }).run();
+                    fs.ensureDir(Path.dirname(props.output), err => {
+                        if (err)
+                            return reject(err);
 
-                    onCancel(_self.encoder.kill.bind(_self.encoder));
+                        _self.encoder
+                            .on('start', function(commandLine) {
+                                _self.trace('[FFMPEG] Query:', commandLine);
+                            })
+                            .on('error', function(error, stdout, stderr) {
+                                _self.removeDeathListener();
+                                reject(error);
+                            })
+                            .on('end', function(stdout, stderr) {
+                                _self.removeDeathListener();
+                                resolve();
+                            }).run();
+                    });
                 });
             });
-        }).return({});
+        }
     };
     static options() {
         return {
@@ -81,6 +101,25 @@ module.exports = class Sample extends nmmes.Module {
                 default: 'middle',
                 describe: 'Milliseconds/percent to start preview at. Middle will take a preview of the middle of the video.',
                 type: 'string',
+                group: 'Advanced:'
+            },
+            'enable': {
+                default: true,
+                describe: 'Create a sample of the finished encode.',
+                type: 'boolean',
+                group: 'General:'
+            },
+            'screenshots': {
+                default: 0,
+                describe: 'Create a number of screenshots of the finished encode. Set to 0 to disable.',
+                type: 'number',
+                group: 'General:'
+            },
+            'screenshot-format': {
+                default: 'jpeg',
+                describe: 'Image format of produced screenshots',
+                type: 'string',
+                choices: ['jpeg', 'png', 'bmp'],
                 group: 'Advanced:'
             },
             'suffix': {
